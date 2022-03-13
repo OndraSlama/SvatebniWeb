d6b6381b589d1bf7d8d18d0fe1773ba3eb94f96c
Ondřej Slama <ondra.slama1@gmail.com>
2022-03-13 22:58:35 +0100

    Improved form, finally connected to sheet

--- src/components/ConfirmationForm.vue
+++ src/components/ConfirmationForm.vue
@@ -1,48 +1,53 @@
 <template>
-	<form id="app" @submit.prevent="sendForm">
-		<div class="form-line">
-			<span :key="1">Přijdu já</span>
-			<input :key="2" v-model="data.name" type="text" placeholder="Jméno a příjmení" class="form-input" />
-			<span :key="3"> s {{ data.isAlone ? "žádnou" : "" }} další{{ data.isPlural ? "mi" : "" }} </span>
-			<input :key="4" v-model="data.totalNumber" type="number" class="form-input w-12" />
-			<span :key="5"> {{ data.isPlural ? "lidmi" : "osobou" }}.</span>	
-				<span v-if="!data.isAlone" class="form-inner-line">
-				<span> Z toho </span>
-				<input v-model="data.childNumber" type="number" class="form-input w-12" />
-				<span> {{ !data.childNumber ? "žádné ne" : "" }}bud{{ data.childNumber > 1 ? "ou děti" : "e dítě" }}. </span>
-			</span>
-			<span :key="6"> Přijed{{ data.isAlone ? "u" : "eme" }} </span>
-			<select :key="7" v-model="data.arrival" class="form-input">
+	<form @submit.prevent="sendForm" class="w-full text-center p-6">
+		<div class="form-line text-center justify-center mb-3 text-lg">
+			<span>Tímto potvrzuji, že přijdu já </span>
+			<input v-model="data.name" type="text" placeholder="Jméno a příjmení" class="form-input w-40" />
+			<span> s </span>
+			<input v-model="data.totalNumber" type="number" placeholder="počet" class="form-input w-16" />
+			<span> {{ data.isAlone ? "žádnou" : "" }} další{{ data.isPlural ? "mi" : "" }} </span>
+			<span> {{ data.isPlural ? "lidmi" : "osobou" }}.</span>	
+			<span v-if="!data.isAlone"> Z toho </span>
+			<input v-if="!data.isAlone" v-model="data.childNumber" type="number" placeholder="počet" class="form-input w-16" />
+			<span  v-if="!data.isAlone"> {{ !data.childNumber ? "žádné ne" : "" }}bud{{ data.childNumber > 1 ? "ou děti" : "e dítě" }}. </span>
+			<span class="basis-full h-0"></span>
+			<span> Přijed{{ data.isAlone ? "u" : "eme" }} </span>
+			<select v-model="data.arrival" class="form-input">
 				<option value="sraz">na sraz do Horního Smrčného</option>
 				<option value="obrad">až na obřad na Chlumě</option>
 			</select>
-			<span :key="8">.</span>
-			<span :key="9" :class="data.arrival == 'obrad' ? 'hidden' : 'form-inner-line'">
-				<span> odkud </span>
-				<select v-model="data.transportSraz" class="form-input">
-					<option value="alone">se dopravím{{ data.isAlone ? " sám" : "e sami" }}</option>
-					<option value="lift">využij{{ data.isAlone ? "i" : "eme" }} odvoz</option>
-				</select>
-				<span> na obřad na Chlum. </span>
-			</span>
-			<span :key="10"> Z Chlumu </span>
+			<span v-if="data.arrival != 'sraz'">.</span> 
+			<span v-if="data.arrival == 'sraz'"> odkud </span>
+			<select v-if="data.arrival == 'sraz'" v-model="data.transportSraz" class="form-input">
+				<option value="alone">se dopravím{{ data.isAlone ? " sám" : "e sami" }}</option>
+				<option value="lift">využij{{ data.isAlone ? "i" : "eme" }} odvoz</option>
+			</select>
+			<span v-if="data.arrival == 'sraz'"> na obřad na Chlum. </span>
+			<span class="basis-full h-0"></span>
+			<span> Z Chlumu </span>
 
-			<select :key="11" v-model="data.transportObrad" class="form-input">
+			<select v-model="data.transportObrad" class="form-input">
 				<option value="alone">se dopravím{{ data.isAlone ? " sám" : "e sami" }}</option>
 				<option value="lift">využij{{ data.isAlone ? "i" : "eme" }} odvoz</option>
 			</select>
 
-			<span :key="12"> na hostinu v Okřížkách. </span>
-			<span :key="13"> Přespání </span>
-			<select :key="14" v-model="data.sleeping" class="form-input">
-				<option value="alone">si zařídím{{data.isAlone ? " sám":"e sami"}}</option>
+			<span> na hostinu v Okřížkách. </span>
+			<span class="basis-full h-0"></span>
+			<span> Přespání </span>
+			<select v-model="data.sleeping" class="form-input">
+				<option value="alone"> zařídit nepotřebuj{{data.isAlone ? "i":"eme"}}</option>
 				<option value="arrange">{{data.isAlone ? "bych potřeboval":"bychom potřebovali"}} zařídit</option>
 			</select>
-			<span :key="15"> . </span>
+			<span v-if="data.sleeping == 'arrange'"> . </span>
+			<span v-if="data.sleeping == 'arrange'">  Proto přidávám kontakt: </span>
+			<input  v-if="data.sleeping == 'arrange'" v-model="data.contact" type="text" placeholder="tel. číslo nebo email" class="form-input" />
+			<span> . </span>
+			<br>
+			<span class="w-full">Vzkaz pro snoubence: </span>
+			<textarea v-model="data.message"  placeholder="Dobrovolné" class="form-input w-full sm:w-1/2" />
 		</div>
-
-
-		<input type="submit" value="Odeslat" class="bg-amber-800 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded-md" />
+		<input :disabled="sendingForm || !formIsValid" type="submit" value="Odeslat" class="bg-amber-800 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded-md disabled:bg-amber-800/30" />
+		<p v-if="!formIsValid" class="text-xs mt-2  text-red-400">Vyplň všechna povinná pole.</p>
 	</form>
 </template>
 
@@ -59,18 +64,84 @@ function initializeForm() {
 		transportSraz: null,
 		transportObrad: null,
 		sleeping: null,
-		text: "",
+		contact: "",
+		message: "",
 		isPlural: computed(() => data.value.totalNumber > 1),
 		isAlone: computed(() => !data.value.totalNumber),
 	};
 }
 initializeForm();
 
+// Watch child number
+watch(() => data.value.childNumber, (newValue, oldValue) => {
+	if (newValue > data.value.totalNumber) {
+		data.value.childNumber = oldValue;
+	}
+});
+
+let sendingForm = ref(false);
+
+
+const formIsValid = computed(() => {
+	if (!(data.value.name 
+		&& data.value.arrival 
+		&& data.value.transportObrad
+		&& data.value.sleeping)){
+		return false;
+	}
+
+	if (data.value.arrival == 'sraz'){
+		if (!data.value.transportSraz){
+			return false;
+		}
+	}
+
+	if (data.value.sleeping == 'arrange'){
+		if (!data.value.contact){
+			return false;
+		}
+	}
+
+	return true;
+});
+
 function sendForm() {
-	console.log("sendForm");
-	console.log(data.value);
-	initializeForm();
+	sendingForm.value = true;
+
+	console.log(formIsValid.value);
+
+	if (!formIsValid.value){
+		alert("Vyplň prosím všechna povinná pole.");
+		sendingForm.value = false;
+		return;
+	}
+
+	var requestOptions = {
+		method: 'POST',
+		body: JSON.stringify({"test": "testing from vue"}),
+	};
+	fetch("https://script.google.com/macros/s/AKfycbxENhogo4bjFgBq7tu0u2q5_EZg3wotw7orbsNKNe1SeclhWWRVfmZjlvwhzmnE51Y/exec", requestOptions)
+		.then(response => response.text())
+		.then(result => {
+			console.log(result);			
+			alert("Děkuji za odeslání formuláře. Potvrzení účasti úspěšně odesláno.");
+		})
+		.catch(error => {
+			console.log(error);
+			alert("Omlouváme se, ale něco se pokazilo. Potvrzení účasti nebylo přijato. Zkus to prosím znovu.");
+		})
+		.finally(() => {
+			sendingForm.value = false;
+		});
 }
+
+function onlyNumber($event) {
+   let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
+   if ((keyCode < 49 || keyCode > 57)) { // 46 is dot
+      $event.preventDefault();
+   }
+}
+
 </script>
 
 <style>
@@ -78,36 +149,21 @@ function sendForm() {
 	@apply flex place-items-center gap-2;
 }
 .form-line {
-	@apply flex place-items-center gap-2 py-2 border-2  flex-wrap;
+	@apply flex place-items-center gap-2 py-2 flex-wrap;
 }
 
 .form-input {
 	@apply border-2 border-amber-700 border-opacity-40 rounded-md focus:border-red-700 px-2;
 }
 
-/* .v-enter-active {
-  transition: opacity 0.5s ease;
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
 }
-
-.v-enter-from{
-  opacity: 0;
-} */
-
-/* .list-move {
-  transition: transform 1s;
+input[type="number"] {
+    -moz-appearance: textfield;
 }
-.list-item {
-  margin-right: 10px;
-}
-.list-item:hover {
-  background-color: wheat;
-}
-.list-enter-active, .list-leave-active {
-  transition: all 0.5s ease;
-}
-.list-enter, .list-leave-to  {
-  opacity: 0;
-} */
 
 
 </style>
