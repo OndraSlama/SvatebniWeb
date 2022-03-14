c514374aca833eadcce066177e03da5d2b6aaf7b
Ondřej Slama <ondra.slama1@gmail.com>
2022-03-14 23:25:18 +0100

    Sending real data to sheet, minor changes

--- src/components/ConfirmationForm.vue
+++ src/components/ConfirmationForm.vue
@@ -5,13 +5,13 @@
 			<input v-model="data.name" type="text" placeholder="Jméno a příjmení" class="form-input w-40" />
 			<span> s </span>
 			<input v-model="data.totalNumber" type="number" placeholder="počet" class="form-input w-16" />
-			<span> {{ data.isAlone ? "žádnou" : "" }} další{{ data.isPlural ? "mi" : "" }} </span>
-			<span> {{ data.isPlural ? "lidmi" : "osobou" }}.</span>	
-			<span v-if="!data.isAlone"> Z toho </span>
-			<input v-if="!data.isAlone" v-model="data.childNumber" type="number" placeholder="počet" class="form-input w-16" />
-			<span  v-if="!data.isAlone"> {{ !data.childNumber ? "žádné ne" : "" }}bud{{ data.childNumber > 1 ? "ou děti" : "e dítě" }}. </span>
+			<span> {{ isAlone ? "žádnou" : "" }} další{{ isPlural ? "mi" : "" }} </span>
+			<span> {{ isPlural ? "lidmi" : "osobou" }}.</span>	
+			<span v-if="!isAlone"> Z toho </span>
+			<input v-if="!isAlone" v-model="data.childNumber" type="number" placeholder="počet" class="form-input w-16" />
+			<span  v-if="!isAlone"> {{ !data.childNumber ? "žádné ne" : "" }}bud{{ data.childNumber > 1 ? "ou děti" : "e dítě" }}. </span>
 			<span class="basis-full h-0"></span>
-			<span> Přijed{{ data.isAlone ? "u" : "eme" }} </span>
+			<span> Přijed{{ isAlone ? "u" : "eme" }} </span>
 			<select v-model="data.arrival" class="form-input">
 				<option value="sraz">na sraz do Horního Smrčného</option>
 				<option value="obrad">až na obřad na Chlumě</option>
@@ -19,24 +19,24 @@
 			<span v-if="data.arrival != 'sraz'">.</span> 
 			<span v-if="data.arrival == 'sraz'"> odkud </span>
 			<select v-if="data.arrival == 'sraz'" v-model="data.transportSraz" class="form-input">
-				<option value="alone">se dopravím{{ data.isAlone ? " sám" : "e sami" }}</option>
-				<option value="lift">využij{{ data.isAlone ? "i" : "eme" }} odvoz</option>
+				<option value="alone">se dopravím{{ isAlone ? " sám" : "e sami" }}</option>
+				<option value="arrange">využij{{ isAlone ? "i" : "eme" }} odvoz</option>
 			</select>
 			<span v-if="data.arrival == 'sraz'"> na obřad na Chlum. </span>
 			<span class="basis-full h-0"></span>
 			<span> Z Chlumu </span>
 
 			<select v-model="data.transportObrad" class="form-input">
-				<option value="alone">se dopravím{{ data.isAlone ? " sám" : "e sami" }}</option>
-				<option value="lift">využij{{ data.isAlone ? "i" : "eme" }} odvoz</option>
+				<option value="alone">se dopravím{{ isAlone ? " sám" : "e sami" }}</option>
+				<option value="arrange">využij{{ isAlone ? "i" : "eme" }} odvoz</option>
 			</select>
 
 			<span> na hostinu v Okřížkách. </span>
 			<span class="basis-full h-0"></span>
 			<span> Přespání </span>
 			<select v-model="data.sleeping" class="form-input">
-				<option value="alone"> zařídit nepotřebuj{{data.isAlone ? "i":"eme"}}</option>
-				<option value="arrange">{{data.isAlone ? "bych potřeboval":"bychom potřebovali"}} zařídit</option>
+				<option value="alone"> zařídit nepotřebuj{{isAlone ? "i":"eme"}}</option>
+				<option value="arrange">{{isAlone ? "bych potřeboval":"bychom potřebovali"}} zařídit</option>
 			</select>
 			<span v-if="data.sleeping == 'arrange'"> . </span>
 			<span v-if="data.sleeping == 'arrange'">  Proto přidávám kontakt: </span>
@@ -44,10 +44,10 @@
 			<span> . </span>
 			<br>
 			<span class="w-full">Vzkaz pro snoubence: </span>
-			<textarea v-model="data.message"  placeholder="Dobrovolné" class="form-input w-full sm:w-1/2" />
+			<textarea v-model="data.message"  placeholder="Např: Chci tatarák a míša řezy. Pivo jedině Bernard." class="form-input w-full sm:w-1/2" />
 		</div>
 		<input :disabled="sendingForm || !formIsValid" type="submit" value="Odeslat" class="bg-amber-800 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded-md disabled:bg-amber-800/30" />
-		<p v-if="!formIsValid" class="text-xs mt-2  text-red-400">Vyplň všechna povinná pole.</p>
+		<p v-if="!formIsValid && !formSent" class="text-xs mt-2  text-red-400">Vyplň všechna povinná pole.</p>
 	</form>
 </template>
 
@@ -66,11 +66,13 @@ function initializeForm() {
 		sleeping: null,
 		contact: "",
 		message: "",
-		isPlural: computed(() => data.value.totalNumber > 1),
-		isAlone: computed(() => !data.value.totalNumber),
+		
 	};
 }
 initializeForm();
+
+let isPlural = computed(() => data.value.totalNumber > 1)
+let isAlone = computed(() => !data.value.totalNumber)
 
 // Watch child number
 watch(() => data.value.childNumber, (newValue, oldValue) => {
@@ -79,7 +81,16 @@ watch(() => data.value.childNumber, (newValue, oldValue) => {
 	}
 });
 
-let sendingForm = ref(false);
+// Watch total number
+watch(() => data.value.totalNumber, newValue => {
+	if (newValue == "") return
+	if (newValue < data.value.childNumber) {
+		data.value.childNumber = newValue;
+	}
+});
+
+const formSent = ref(false);
+const sendingForm = ref(false);
 
 
 const formIsValid = computed(() => {
@@ -108,8 +119,6 @@ const formIsValid = computed(() => {
 function sendForm() {
 	sendingForm.value = true;
 
-	console.log(formIsValid.value);
-
 	if (!formIsValid.value){
 		alert("Vyplň prosím všechna povinná pole.");
 		sendingForm.value = false;
@@ -118,13 +127,19 @@ function sendForm() {
 
 	var requestOptions = {
 		method: 'POST',
-		body: JSON.stringify({"test": "testing from vue"}),
+		body: JSON.stringify(data.value),
 	};
 	fetch("https://script.google.com/macros/s/AKfycbxENhogo4bjFgBq7tu0u2q5_EZg3wotw7orbsNKNe1SeclhWWRVfmZjlvwhzmnE51Y/exec", requestOptions)
 		.then(response => response.text())
 		.then(result => {
-			console.log(result);			
-			alert("Děkuji za odeslání formuláře. Potvrzení účasti úspěšně odesláno.");
+			console.log(result);	
+			if(result == "Error"){
+				alert("Omlouváme se, ale něco se pokazilo. Potvrzení účasti se nedalo zpracovat. Zkontroluj data a zkus to prosím znovu.");
+			}else{
+				alert("Děkuji za odeslání formuláře. Potvrzení účasti úspěšně odesláno.");
+				formSent.value = true;
+				initializeForm()
+			}
 		})
 		.catch(error => {
 			console.log(error);
