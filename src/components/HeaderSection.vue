ce6c70c2e1934373a820df398fe9c51e000b1523
Ondrej Slama <ondrej.slama@rossum.ai>
2022-03-25 22:28:48 +0100

    Improved header section, added custom font

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,7 +1,13 @@
 <template>
-	<v-parallax max-height="70vh" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
-			<h1 >H&O</h1>
-			<p>27.8.2022</p>
+	<v-parallax scale="1.5" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" class="tw-flex align-center tw-max-h-screen tw-min-h-screen">
+			<div class="tw-text-center tw-font-[hobbiton]">
+				<div class="tw-mb-10 tw-text-8xl md:tw-text-9xl tw-flex lg:tw-gap-8 tw-justify-center tw-flex-col lg:tw-flex-row">
+					<div>Hana</div>
+					<div>&</div>
+					<div>Ondra</div>
+				</div>
+				<h3 class="tw-text-5xl md:tw-text-7xl">27.8.2022</h3>
+			</div>
 	</v-parallax>
 </template>
 
